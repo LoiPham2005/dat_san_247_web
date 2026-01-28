1515b796715bb9b8edeb875bcf3057cd712e6186
đức lợi phạm <loipd@vtn-global.com>
2026-01-29 00:49:37 +0700

    gggggggggg

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  eslint: {
-    // Bỏ qua lỗi ESLint khi build
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Bỏ qua lỗi Type check khi build
-    ignoreBuildErrors: true,
-  }
 };
 
 export default nextConfig;
